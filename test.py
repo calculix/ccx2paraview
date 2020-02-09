b1f8fdc7ffbebc0d3d5409898f68f6026f11383f
imirzov <imirzov@gmail.com>
2020-02-09 19:16:34 +0100

    Performed new test run

--- test.py
+++ test.py
@@ -96,9 +96,9 @@ if (__name__ == '__main__'):
     # Enable multithreading for CalculiX
     os.environ['OMP_NUM_THREADS'] = str(os.cpu_count())
 
-    tests_folder = os.path.join(os.path.dirname(__file__), 'examples')
-    # run_all_analyses_in(tests_folder)
-    test_frd_parser_on_models_in(tests_folder)
-    # convert_calculation_results_in(tests_folder)
+    folder = os.path.join(os.path.dirname(__file__), 'examples')
+    # run_all_analyses_in(folder)
+    # test_frd_parser_on_models_in(folder)
+    convert_calculation_results_in(folder)
 
     clean.cache()
